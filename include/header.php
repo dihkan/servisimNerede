01dc01c026a318565d1c93424508dbee5ef9c9d5
dihkan <dihkan@gmail.com>
2022-12-09 15:29:59 +0300

    Start

---
+++ include/header.php
@@ -0,0 +1,27 @@
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+      <div class="container d-flex align-items-center justify-content-between">
+
+          <div class="logo">
+              <h1><a href="index.html">Servisim <span class="text-info">Nerede?</span> </a></h1>
+              <!-- Uncomment below if you prefer to use an image logo -->
+              <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+          </div>
+
+          <nav id="navbar" class="navbar">
+              <ul>
+                  <li><a class="nav-link scrollto active" href="#hero">Ana Sayfa</a></li>
+                  <li><a class="nav-link scrollto" href="#services">Araç Kayıt</a></li>
+                  <li><a class="nav-link scrollto " href="#portfolio">Veli Giriş</a></li>
+
+                  <li><a class="nav-link scrollto" href="#team">Takımımız</a></li>
+                  <li><a class="nav-link scrollto" href="#pricing">SSS</a></li>
+
+                  <li><a class="nav-link scrollto" href="#team">İletişim</a></li>
+
+              </ul>
+              <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav><!-- .navbar -->
+
+      </div>
+  </header><!-- End Header -->
---
+++ index.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once "include/head.php"; ?>
+
+<body>
+
+
+    <?php require_once "include/header.php"; ?>
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+                    <h1 data-aos="fade-up">Aklınız çocuğunuzda !</h1>
+                    <h2 data-aos="fade-up" data-aos-delay="400">Biliyoruz. "Servisim <span class="text-info"> Nerede? </span>" uygulamasını bu sebeple ürettik... </h2>
+                    <div data-aos="fade-up" data-aos-delay="800">
+                        <a href="#about" class="btn-get-started scrollto">Kayıt Ol!</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
+                    <img src="assets/img/servis3.png" class="img-fluid animated" alt="">
+                </div>
+            </div>
+        </div>
+
+    </section><!-- End Hero -->
+
+    <main id="main">
+
+        <!-- ======= Clients Section ======= -->
+        <section id="clients" class="clients clients">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-lg-2 col-md-4 col-6">
+
+                    </div>
+                    <div class="col-lg-2 col-md-4 col-6">
+                        <img src="assets/img/okul.png" class="img-fluid" alt="" data-aos="zoom-in">
+                    </div>
+
+
+
+                    <div class="col-lg-2 col-md-4 col-6">
+
+                    </div>
+
+                    <div class="col-lg-2 col-md-4 col-6">
+
+                    </div>
+
+
+                    <div class="col-lg-2 col-md-4 col-6">
+                        <img src="assets/img/sn.png" class="img-fluid" alt="" data-aos="zoom-in" data-aos-delay="500">
+                    </div>
+                    <div class="col-lg-2 col-md-4 col-6">
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Clients Section -->
+
+
+
+
+        <!-- ======= Team Section ======= -->
+        <section id="team" class="team section-bg">
+            <div class="container">
+
+                <div class="section-title" data-aos="fade-up">
+                    <h2>TeknoKafalar</h2>
+                    <p>Günümüz problemlerine çözüm üretmek tek idealimiz...</p>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                        <div class="member" data-aos="fade-up" data-aos-delay="100">
+                            <div class="member-img">
+                                <img src="assets/img/team/tk1.jpg" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href="http://twitter.com/"><i class="bi bi-twitter"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>Alpay Burak BİRİNCİ</h4>
+                                <span>Backend JR.</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                        <div class="member" data-aos="fade-up" data-aos-delay="200">
+                            <div class="member-img">
+                                <img src="assets/img/team/tk2.jpeg" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>Yağız Alp SÜRMENELİ</h4>
+                                <span> Backend JR. </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                        <div class="member" data-aos="fade-up" data-aos-delay="300">
+                            <div class="member-img">
+                                <img src="assets/img/team/tk3.jpeg" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>Yağızhan SAYAR</h4>
+                                <span>Mobil JR. </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                        <div class="member" data-aos="fade-up" data-aos-delay="400">
+                            <div class="member-img">
+                                <img src="assets/img/team/tk4.jpeg" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>Yiğit Alp SÜRMENELİ</h4>
+                                <span>Frontend JR.</span>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Team Section -->
+
+
+        <!-- ======= F.A.Q Section ======= -->
+        <section id="faq" class="faq">
+            <div class="container">
+
+                <div class="section-title" data-aos="fade-up">
+                    <h2>Sıkça Sorulan Sorular</h2>
+                </div>
+
+                <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-5">
+                        <i class="ri-question-line"></i>
+                        <h4>Tam olarak çocuğumu nasıl izleyeceğim?</h4>
+                    </div>
+                    <div class="col-lg-7">
+                        <p>
+                            Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
+                        </p>
+                    </div>
+                </div><!-- End F.A.Q Item-->
+
+                <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+                    <div class="col-lg-5">
+                        <i class="ri-question-line"></i>
+                        <h4>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</h4>
+                    </div>
+                    <div class="col-lg-7">
+                        <p>
+                            Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim.
+                        </p>
+                    </div>
+                </div><!-- End F.A.Q Item-->
+
+                <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+                    <div class="col-lg-5">
+                        <i class="ri-question-line"></i>
+                        <h4>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?</h4>
+                    </div>
+                    <div class="col-lg-7">
+                        <p>
+                            Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus.
+                        </p>
+                    </div>
+                </div><!-- End F.A.Q Item-->
+
+                <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
+                    <div class="col-lg-5">
+                        <i class="ri-question-line"></i>
+                        <h4>Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?</h4>
+                    </div>
+                    <div class="col-lg-7">
+                        <p>
+                            Aperiam itaque sit optio et deleniti eos nihil quidem cumque. Voluptas dolorum accusantium sunt sit enim. Provident consequuntur quam aut reiciendis qui rerum dolorem sit odio. Repellat assumenda soluta sunt pariatur error doloribus fuga.
+                        </p>
+                    </div>
+                </div><!-- End F.A.Q Item-->
+
+                <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="500">
+                    <div class="col-lg-5">
+                        <i class="ri-question-line"></i>
+                        <h4>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</h4>
+                    </div>
+                    <div class="col-lg-7">
+                        <p>
+                            Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
+                        </p>
+                    </div>
+                </div><!-- End F.A.Q Item-->
+
+            </div>
+        </section><!-- End F.A.Q Section -->
+
+        <!-- ======= Contact Section ======= -->
+        <section id="contact" class="contact">
+            <div class="container">
+
+                <div class="section-title" data-aos="fade-up">
+                    <h2>Contact Us</h2>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                        <div class="contact-about">
+                            <h3>Vesperr</h3>
+                            <p>Cras fermentum odio eu feugiat. Justo eget magna fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
+                            <div class="social-links">
+                                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
+                        <div class="info">
+                            <div>
+                                <i class="ri-map-pin-line"></i>
+                                <p>A108 Adam Street<br>New York, NY 535022</p>
+                            </div>
+
+                            <div>
+                                <i class="ri-mail-send-line"></i>
+                                <p>info@example.com</p>
+                            </div>
+
+                            <div>
+                                <i class="ri-phone-line"></i>
+                                <p>+1 5589 55488 55s</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-lg-5 col-md-12" data-aos="fade-up" data-aos-delay="300">
+                        <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                            <div class="form-group">
+                                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                            </div>
+                            <div class="my-3">
+                                <div class="loading">Loading</div>
+                                <div class="error-message"></div>
+                                <div class="sent-message">Your message has been sent. Thank you!</div>
+                            </div>
+                            <div class="text-center"><button type="submit">Send Message</button></div>
+                        </form>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Contact Section -->
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="container">
+            <div class="row d-flex align-items-center">
+                <div class="col-lg-6 text-lg-left text-center">
+                    <div class="copyright">
+                        &copy; Copyright <strong>Vesperr</strong>. All Rights Reserved
+                    </div>
+                    <div class="credits">
+                        <!-- All the links in the footer should remain intact. -->
+                        <!-- You can delete the links only if you purchased the pro version. -->
+                        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/vesperr-free-bootstrap-template/ -->
+                        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <nav class="footer-links text-lg-right text-center pt-2 pt-lg-0">
+                        <a href="#intro" class="scrollto">Home</a>
+                        <a href="#about" class="scrollto">About</a>
+                        <a href="#">Privacy Policy</a>
+                        <a href="#">Terms of Use</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <?php require_once "include/js.php"; ?>
+
+</body>
+
+</html>
